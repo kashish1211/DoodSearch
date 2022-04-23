ad723f8588f526d4cb3c624e597e7d3678c97001
shubh0208 <62873923+shubh0208@users.noreply.github.com>
2022-04-23 15:41:45 +0530

    file structure changed

--- test.py
+++
@@ -1,9 +0,0 @@
-import cv2
-window_name = 'image'
-im = cv2.imread('opencv_frame.png')
-im = cv2.resize(im,(96,96))
-cv2.imshow(window_name,im)
-cv2.waitKey(0) 
-  
-#closing all open windows 
-cv2.destroyAllWindows() 
